31003640db9e185e8047017bde61ff366385b437
amberkakkar01 <amberkakkar01@gmail.com>
2021-10-23 21:39:26 +0530

    Added ejs files and Updated controllers

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -4,3 +4,18 @@ module.exports.profile = function(req, res){
     title: "Users Profile!!"
   });
 }
+
+//render the sign up page
+module.exports.signUp = function(req,res){
+  return res.render('user_sign_up',{
+    title: "Codeial | Sign Up"
+  })
+
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+  return res.render('user_sign_in',{
+    title: "Codeial | Sign In" 
+  })
+}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const userController = require('../controllers/users_controllers')
 
 router.get('/profile',userController.profile)
+router.get('/sign-up', userController.signUp)
+router.get('/sign-in', userController.signIn)
  
 module.exports = router;
 
