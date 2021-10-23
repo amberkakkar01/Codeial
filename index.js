d82ebccd5bbade0c0040cb2a5997bf8b50d5318f
amberkakkar01 <amberkakkar01@gmail.com>
2021-10-23 19:37:07 +0530

    Added Views

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const port =8000;
+const port = 8000;
 
 //use express route
 app.use('/',require('./routes'));
 
 app.set('view engine','ejs');
-app.set('view','./views');
+app.set('views','./views');
+app.set('views','./views');
 
 app.listen(port,function(err){
   if(err)
