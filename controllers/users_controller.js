c77e97a2c2f81b5abdbe542544aa58e80a3d4c0e
amberkakkar01 <amberkakkar01@gmail.com>
2021-10-24 13:04:28 +0530

    Added Authorization for user sign-in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -52,4 +52,33 @@ module.exports.create = function(req,res){
 //sign in and create session
 module.exports.createSession = function(req,res){
 
+    // find the user
+    User.findOne({email: req.body.email}, function(err,user){
+
+        if(err){
+            console.log('Error in finding user in signing in');
+            return
+        }
+        // handle user found
+        if(user){
+
+            // handle password which don't match
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+            // handle session creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+        }
+        else
+        {
+            // handle user not found
+            return res.redirect('back');
+        }
+
+    });
+    
+    
+    
+    
 }
