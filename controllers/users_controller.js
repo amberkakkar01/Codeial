49b8399ec1d51430cec54a1d8c128361f11d8ad8
amberkakkar01 <amberkakkar01@gmail.com>
2021-10-29 11:29:09 +0530

    Added Sign Out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,6 +9,9 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('./users/profile');
+    }
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
@@ -17,6 +20,11 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+
+    if(req.isAuthenticated()){
+       return res.redirect('./users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
@@ -52,5 +60,11 @@ module.exports.create = function(req,res){
 //sign in and create session
 module.exports.createSession = function(req,res){
 
+    return res.redirect('/');
+}
+
+//Log-out
+module.exports.destroySession = function(req, res){
+    req.logout();
     return res.redirect('/');
 }
