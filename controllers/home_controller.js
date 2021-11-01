f4c4b337b6fb7092b23eb9c2807c1b269851cd32
amberkakkar01 <amberkakkar01@gmail.com>
2021-11-01 11:38:29 +0530

    Displaying Posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,23 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
     
-    return res.render('home', {
-        title: "Home"
-    });
+   // Post.find({}, function(err, posts){
+
+    //    return res.render('home', {
+   //         title: "Codeial | Home",
+   //         posts: posts
+  //      });
+  //  });
+    
+    //finding use of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    })
 }
 
 // module.exports.actionName = function(req, res){}
